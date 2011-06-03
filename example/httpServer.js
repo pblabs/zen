17241d244ea6b82ddcb7314e7e0bd0977afdc79f
pibi <pibi@pianobit.com>
2011-06-03 10:06:44 +0200

    less trivial http example

--- example/httpServer.js
+++ example/httpServer.js
@@ -1,7 +1,8 @@
 var zen=require('../zen');
 var hello=function(request,response,next){next(null,"hello world!");}
+var first=function(request,response,next){process.nextTick(function(){next()});};
 
-var stack=zen(hello);
+var stack=zen(first,hello);
 
 stack.resultHandler = function result(request,response, res) {
 	response.writeHead(200,{'Content-type':'text/plain'})
@@ -10,6 +11,7 @@ stack.resultHandler = function result(request,response, res) {
 };
 
 stack.errorHandler = function result(request,response, err) {
+	console.log("NEXTED!",err)
 	if (err) {
 		response.writeHead(500,{'Content-type':'text/plain'})
 		response.end(err.stack||err);
@@ -18,7 +20,6 @@ stack.errorHandler = function result(request,response, err) {
 	response.end('Not Found');
     return;
 };
-
 
 require('http').createServer(stack).listen(8080);
 console.log('app listen on port 8080');
